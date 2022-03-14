5003d4a1f06122604f41cbc0f8a9410d83f0190b
Will Hernandez <w.hernandez.code@gmail.com>
2022-03-14 08:27:05 -0400

    removed additional secret_key

--- currencyapi/settings.py
+++ currencyapi/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-SECRET_KEY=os.environ.get('SECRET_KEY')
-
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
