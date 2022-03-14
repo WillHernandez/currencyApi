e37d7dcef610ebd3e71ef6c36ab2d2cadae6729d
Will Hernandez <w.hernandez.code@gmail.com>
2022-03-14 09:03:34 -0400

    Heroku url added to allowed_hosts

--- currencyapi/settings.py
+++ currencyapi/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1',
+    'localhost', 'https://currency-api-will-h.herokuapp.com/'
+]
 
 
 # Application definition
